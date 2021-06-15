3a7c5b6d623166686860a378a572d174bab98690
JAYAPRIYA28 <83283403+JAYAPRIYA28@users.noreply.github.com>
2021-06-15 22:20:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,73 @@
 import cv2
 import numpy as np
 
-# cam = cv2.VideoCapture(0)
-#
-# cv2.namedWindow("test")
-#
-# img_counter = 0
-#
-# while True:
-#     ret, frame = cam.read()
-#     if not ret:
-#         print("failed to grab frame")
-#         break
-#     cv2.imshow("test", frame)
-#
-#     k = cv2.waitKey(1)
-#     if k%256 == 27:
-#
-#         print("Escape hit, closing...")
-#         break
-#     elif k%256 == 32:
-#
-#         img_name = "opencv_frame_{}.png".format(img_counter)
-#         cv2.imwrite(img_name, frame)
-#         print("{} image stored!".format(img_name))
-#         path = img_name
-#         img_counter += 1
-#
-# cam.release()
-#
-# cv2.destroyAllWindows()
-#
-#
-# def empty(a):
-#     pass
-#
-# cv2.namedWindow("TrackBars")
-# cv2.resizeWindow("TrackBars",640,240)
-# cv2.createTrackbar("Hue Min","TrackBars",0,179,empty)
-# cv2.createTrackbar("Hue Max","TrackBars",19,179,empty)
-# cv2.createTrackbar("Sat Min","TrackBars",110,255,empty)
-# cv2.createTrackbar("Sat Max","TrackBars",240,255,empty)
-# cv2.createTrackbar("Val Min","TrackBars",153,255,empty)
-# cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
-# while True:
-#     img = cv2.imread(path)
-#     imgResize = cv2.resize(img,(462, 462))
-#     imgHSV = cv2.cvtColor(imgResize,cv2.COLOR_BGR2HSV)
-#     h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
-#     h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
-#     s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
-#     s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
-#     v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
-#     v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
-#     print(h_min,h_max,s_min,s_max,v_min,v_max)
-#     lower = np.array([h_min,s_min,v_min])
-#     upper = np.array([h_max,s_max,v_max])
-#     mask = cv2.inRange(imgHSV,lower,upper)
-#     imgResult = cv2.bitwise_and(imgResize,imgResize,mask=mask)
-#
-#
-#     # cv2.imshow("Original",img)
-#     # cv2.imshow("HSV",imgHSV)
-#     cv2.imshow("Mask", mask)
-#     cv2.imshow("Result", imgResult)
-#
-#
-#
-#     cv2.waitKey(1)
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} image stored!".format(img_name))
+        path = img_name
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
+
+
+def empty(a):
+    pass
+
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars",640,240)
+cv2.createTrackbar("Hue Min","TrackBars",0,179,empty)
+cv2.createTrackbar("Hue Max","TrackBars",19,179,empty)
+cv2.createTrackbar("Sat Min","TrackBars",110,255,empty)
+cv2.createTrackbar("Sat Max","TrackBars",240,255,empty)
+cv2.createTrackbar("Val Min","TrackBars",153,255,empty)
+cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
+while True:
+    img = cv2.imread(path)
+    imgResize = cv2.resize(img,(462, 462))
+    imgHSV = cv2.cvtColor(imgResize,cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    print(h_min,h_max,s_min,s_max,v_min,v_max)
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    mask = cv2.inRange(imgHSV,lower,upper)
+    imgResult = cv2.bitwise_and(imgResize,imgResize,mask=mask)
+
+
+    # cv2.imshow("Original",img)
+    # cv2.imshow("HSV",imgHSV)
+    cv2.imshow("Mask", mask)
+    cv2.imshow("Result", imgResult)
+
+
+
+    cv2.waitKey(1)
 
 #______________________videocapture_______________________________#
 
