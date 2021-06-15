16f3958a337336d5ea082cba52e38801da3042c2
JAYAPRIYA28 <83283403+JAYAPRIYA28@users.noreply.github.com>
2021-06-15 22:18:08 +0530

    add file

---
+++ main.py
@@ -0,0 +1,149 @@
+import cv2
+import numpy as np
+
+# cam = cv2.VideoCapture(0)
+#
+# cv2.namedWindow("test")
+#
+# img_counter = 0
+#
+# while True:
+#     ret, frame = cam.read()
+#     if not ret:
+#         print("failed to grab frame")
+#         break
+#     cv2.imshow("test", frame)
+#
+#     k = cv2.waitKey(1)
+#     if k%256 == 27:
+#
+#         print("Escape hit, closing...")
+#         break
+#     elif k%256 == 32:
+#
+#         img_name = "opencv_frame_{}.png".format(img_counter)
+#         cv2.imwrite(img_name, frame)
+#         print("{} image stored!".format(img_name))
+#         path = img_name
+#         img_counter += 1
+#
+# cam.release()
+#
+# cv2.destroyAllWindows()
+#
+#
+# def empty(a):
+#     pass
+#
+# cv2.namedWindow("TrackBars")
+# cv2.resizeWindow("TrackBars",640,240)
+# cv2.createTrackbar("Hue Min","TrackBars",0,179,empty)
+# cv2.createTrackbar("Hue Max","TrackBars",19,179,empty)
+# cv2.createTrackbar("Sat Min","TrackBars",110,255,empty)
+# cv2.createTrackbar("Sat Max","TrackBars",240,255,empty)
+# cv2.createTrackbar("Val Min","TrackBars",153,255,empty)
+# cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
+# while True:
+#     img = cv2.imread(path)
+#     imgResize = cv2.resize(img,(462, 462))
+#     imgHSV = cv2.cvtColor(imgResize,cv2.COLOR_BGR2HSV)
+#     h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+#     h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+#     s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+#     s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+#     v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+#     v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+#     print(h_min,h_max,s_min,s_max,v_min,v_max)
+#     lower = np.array([h_min,s_min,v_min])
+#     upper = np.array([h_max,s_max,v_max])
+#     mask = cv2.inRange(imgHSV,lower,upper)
+#     imgResult = cv2.bitwise_and(imgResize,imgResize,mask=mask)
+#
+#
+#     # cv2.imshow("Original",img)
+#     # cv2.imshow("HSV",imgHSV)
+#     cv2.imshow("Mask", mask)
+#     cv2.imshow("Result", imgResult)
+#
+#
+#
+#     cv2.waitKey(1)
+
+#______________________videocapture_______________________________#
+
+video = cv2.VideoCapture(0)
+
+
+if (video.isOpened() == False):
+    print("Error reading video file")
+
+
+frame_width = int(video.get(3))
+frame_height = int(video.get(4))
+
+size = (frame_width, frame_height)
+
+
+result = cv2.VideoWriter('filename.mp4',cv2.VideoWriter_fourcc(*'XVID'),5, size)
+
+while (True):
+    ret, frame = video.read()
+
+    if ret == True:
+        result.write(frame)
+        cv2.imshow('Frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            break
+    else:
+        break
+
+
+video.release()
+result.release()
+
+
+cv2.destroyAllWindows()
+
+print("The video was successfully saved")
+
+
+cap = cv2.VideoCapture("filename.mp4")
+def empty(a):
+    pass
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars",640,240)
+cv2.createTrackbar("Hue Min","TrackBars",0,179,empty)
+cv2.createTrackbar("Hue Max","TrackBars",19,179,empty)
+cv2.createTrackbar("Sat Min","TrackBars",110,255,empty)
+cv2.createTrackbar("Sat Max","TrackBars",240,255,empty)
+cv2.createTrackbar("Val Min","TrackBars",153,255,empty)
+cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
+
+
+while True:
+    _, img = cap.read()
+
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    print(h_min,h_max,s_min,s_max,v_min,v_max)
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    mask = cv2.inRange(imgHSV,lower,upper)
+    imgResult = cv2.bitwise_and(img,img,mask=mask)
+
+    # cv2.imshow("HSV", imgHSV)
+    cv2.imshow("Mask", mask)
+    cv2.imshow("Result", imgResult)
+    if cv2.waitKey(50) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+
